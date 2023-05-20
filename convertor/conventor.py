ad9c424b67f2ea125050d1f82a3af67cd2772434
mahshidghaffari <m.ghaffari@student.maastrichtuniversity.nl>
2023-05-20 21:02:15 +0200

    add one more

--- convertor/conventor.py
+++ convertor/conventor.py
@@ -1,6 +1,16 @@
+import pandas as pd
+
 def convert_to_text(dataset, name):
     filename = "/home/jovyan/work/data/output/" + name + ".txt"
     words = dataset["word"].astype(str).tolist()
     text = " ".join(words)
     with open(filename, "w") as file:
         file.write(text)
+
+
+def create_dataframe_from_text(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+    words = text.split()
+    df = pd.DataFrame({'word': words})
+    return df
